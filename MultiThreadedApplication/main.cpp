11686d6a3e16b3598b96a452647ad3d4b8921aa3
Artur Jaakman <jaakmanartur@gmail.com>
2018-07-03 23:15:37 +0100

    Mutex

--- MultiThreadedApplication/main.cpp
+++ MultiThreadedApplication/main.cpp
@@ -2,36 +2,45 @@
 #include <thread>
 #include <chrono>
 #include <string>
+#include <mutex>
 
 int iterator = 0;
+std::mutex printLock; // Locks a block of code to prevent multiple threads from accessing shared data simultaniously.
 
-void FirstTask(std::string name)
+void Task(std::string name)
 {		
+	//std::lock_guard<std::mutex> lock(printLock);
 	std::cout << name << " Thread Beginning ID: " << std::this_thread::get_id() << std::endl;
+	
 	for (int i = 0; i < 10; ++i)
 	{
+		printLock.lock();
 		++iterator;
 		std::cout << name << " Thread Working " << iterator << std::endl;
+		printLock.unlock();
 		//std::this_thread::sleep_for(std::chrono::milliseconds(500));
-	}
-	std::cout << name << " Thread End ID: " << std::this_thread::get_id() << std::endl;
+	}	
+	std::cout << name << " Thread End ID: " << std::this_thread::get_id() << std::endl;	
 }
 
 int main()
-{
+{	
 	std::cout << "Main Thread Beginning ID: " << std::this_thread::get_id() << std::endl;
-
-	std::thread FirstThread(FirstTask, "First"); // Creating a thread that runs FirstTask.
+	
+	std::thread FirstThread(Task, "First"); // Creating a thread that runs FirstTask.
 	FirstThread.detach(); // Makes the thread run completely independantly without the need to join it.
-
+	
 	for (int i = 0; i < 10; ++i)
 	{
+		printLock.lock();
 		++iterator;
 		std::cout << "Main Thread Working " << std::to_string(iterator) << std::endl;
+		printLock.unlock();
 		//std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	}
 
 	std::cout << "Main Thread End ID: " << std::this_thread::get_id() << std::endl;
+	
 	
 	//FirstThread.join(); // Waits for the FirstTask on FirstThread to finish before continuing with MainThread.
 	std::cin.get();
