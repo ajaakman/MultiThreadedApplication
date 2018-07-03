731673a1feffba554fea141191be4a726b57e620
Artur Jaakman <jaakmanartur@gmail.com>
2018-07-03 17:16:49 +0100

    thread_id

--- MultiThreadedApplication/main.cpp
+++ MultiThreadedApplication/main.cpp
@@ -3,19 +3,19 @@
 #include <chrono>
 
 void FirstTask()
-{
-	std::cout << "First Thread Beginning\n" << std::flush; // If output buffer isn't flushed the thread keeps working until it's done. If using endl sometimes it doesn't print new line after switching to another thread.
+{	
+	std::cout << "First Thread Beginning ID: " << std::this_thread::get_id() << "\n" << std::flush; // If output buffer isn't flushed the thread keeps working until it's done. If using endl sometimes it doesn't print new line after switching to another thread.
 	for (int i = 0; i < 10; ++i)
 	{
 		std::cout << "First Thread Working\n" << std::flush;
 		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	}
-	std::cout << "First Thread End\n" << std::flush;
+	std::cout << "First Thread End ID: " << std::this_thread::get_id() << "\n" << std::flush;
 }
 
 int main()
 {
-	std::cout << "Main Thread Beginning\n" << std::flush;
+	std::cout << "Main Thread Beginning ID: " << std::this_thread::get_id() << "\n" << std::flush;
 
 	std::thread FirstThread(FirstTask); // Creating a thread that runs FirstTask.
 	
@@ -25,7 +25,7 @@ int main()
 		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	}
 
-	std::cout << "Main Thread End\n" << std::flush;
+	std::cout << "Main Thread End ID: " << std::this_thread::get_id() << "\n" << std::flush;
 	
 	FirstThread.join(); // Waits for the FirstTask on FirstThread to finish before continuing with MainThread.
 	std::cin.get();
