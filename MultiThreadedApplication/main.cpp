cf39756ba22f3ed74373ea9a03bcec9cc145c4f5
Artur Jaakman <jaakmanartur@gmail.com>
2018-07-03 17:01:59 +0100

    sleep_for

--- MultiThreadedApplication/main.cpp
+++ MultiThreadedApplication/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <thread>
+#include <chrono>
 
 void FirstTask()
 {
 	std::cout << "First Thread Beginning\n" << std::flush; // If output buffer isn't flushed the thread keeps working until it's done. If using endl sometimes it doesn't print new line after switching to another thread.
-	for (int i = 0; i < 5; ++i)
+	for (int i = 0; i < 10; ++i)
 	{
 		std::cout << "First Thread Working\n" << std::flush;
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	}
 	std::cout << "First Thread End\n" << std::flush;
 }
@@ -17,9 +19,10 @@ int main()
 
 	std::thread FirstThread(FirstTask); // Creating a thread that runs FirstTask.
 	
-	for (int i = 0; i < 5; ++i)
+	for (int i = 0; i < 10; ++i)
 	{
 		std::cout << "Main Thread Working\n" << std::flush;
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	}
 
 	std::cout << "Main Thread End\n" << std::flush;
