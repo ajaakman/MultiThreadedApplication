3392fc089f1c5d4526e4db8bd3d39114db99eb5c
Artur Jaakman <jaakmanartur@gmail.com>
2018-07-03 23:25:33 +0100

    lock_guard

--- MultiThreadedApplication/main.cpp
+++ MultiThreadedApplication/main.cpp
@@ -9,39 +9,29 @@ std::mutex printLock; // Locks a block of code to prevent multiple threads from 
 
 void Task(std::string name)
 {		
-	//std::lock_guard<std::mutex> lock(printLock);
-	printLock.lock();
 	std::cout << name << " Thread Beginning ID: " << std::this_thread::get_id() << std::endl;
-	printLock.unlock();
 	for (int i = 0; i < 10; ++i)
-	{
-		printLock.lock();
+	{		
+	std::lock_guard<std::mutex> lock(printLock); // Restricts access in scope of this function. Exception safe.
 		++iterator;
-		std::cout << name << " Thread Working " << iterator << std::endl;
-		printLock.unlock();
+		std::cout << name << " Thread Working " << iterator << std::endl;		
 		//std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	}	
-	printLock.lock();
 	std::cout << name << " Thread End ID: " << std::this_thread::get_id() << std::endl;	
-	printLock.unlock();
 }
 
 int main()
-{	
-	printLock.lock();
-	std::cout << "Main Thread Beginning ID: " << std::this_thread::get_id() << std::endl;
-	printLock.unlock();
-
-	std::thread FirstThread(Task, "First"); // Creating a thread that runs FirstTask.
-	FirstThread.detach(); // Makes the thread run completely independantly without the need to join it.
+{		
+	//std::cout << "Main Thread Beginning ID: " << std::this_thread::get_id() << std::endl;
 	
+	std::thread FirstThread(Task, "First"); // Creating a thread that runs FirstTask.
 	std::thread SecondThread(Task, "Second");
+	
+	FirstThread.detach(); // Makes the thread run completely independantly without the need to join it.
 	SecondThread.detach();
 
-	printLock.lock();
-	std::cout << "Main Thread End ID: " << std::this_thread::get_id() << std::endl;
-	printLock.unlock();
-	
+	//std::cout << "Main Thread End ID: " << std::this_thread::get_id() << std::endl;
+		
 	//FirstThread.join(); // Waits for the FirstTask on FirstThread to finish before continuing with MainThread.
 	std::cin.get();
 }
