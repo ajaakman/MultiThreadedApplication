66c82be3e723da5cca23b2b2cbe808f26c1eb35b
Artur Jaakman <jaakmanartur@gmail.com>
2018-07-03 22:57:22 +0100

    Passing an argument on create thread.

--- MultiThreadedApplication/main.cpp
+++ MultiThreadedApplication/main.cpp
@@ -5,35 +5,33 @@
 
 int iterator = 0;
 
-void FirstTask()
+void FirstTask(std::string name)
 {		
-	std::cout << "First Thread Beginning ID: " << std::this_thread::get_id() << "\n" << std::flush; // If output buffer isn't flushed the thread keeps working until it's done. If using endl sometimes it doesn't print new line after switching to another thread.
+	std::cout << name << " Thread Beginning ID: " << std::this_thread::get_id() << std::endl;
 	for (int i = 0; i < 10; ++i)
 	{
 		++iterator;
-		std::string a = "First Thread Working " + std::to_string(iterator) + "\n";
-		std::cout << a << std::flush;
+		std::cout << name << " Thread Working " << iterator << std::endl;
 		//std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	}
-	std::cout << "First Thread End ID: " << std::this_thread::get_id() << "\n" << std::flush;
+	std::cout << name << " Thread End ID: " << std::this_thread::get_id() << std::endl;
 }
 
 int main()
 {
-	std::cout << "Main Thread Beginning ID: " << std::this_thread::get_id() << "\n" << std::flush;
+	std::cout << "Main Thread Beginning ID: " << std::this_thread::get_id() << std::endl;
 
-	std::thread FirstThread(FirstTask); // Creating a thread that runs FirstTask.
+	std::thread FirstThread(FirstTask, "First"); // Creating a thread that runs FirstTask.
 	FirstThread.detach(); // Makes the thread run completely independantly without the need to join it.
 
 	for (int i = 0; i < 10; ++i)
 	{
 		++iterator;
-		std::string a = "Main Thread Working " + std::to_string(iterator) + "\n";
-		std::cout << a << std::flush;
+		std::cout << "Main Thread Working " << std::to_string(iterator) << std::endl;
 		//std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	}
 
-	std::cout << "Main Thread End ID: " << std::this_thread::get_id() << "\n" << std::flush;
+	std::cout << "Main Thread End ID: " << std::this_thread::get_id() << std::endl;
 	
 	//FirstThread.join(); // Waits for the FirstTask on FirstThread to finish before continuing with MainThread.
 	std::cin.get();
